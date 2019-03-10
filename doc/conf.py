c91dd38eb2e85fa189c15d7c20e4c4d83c68facc
joinAero <join.aero@gmail.com>
2019-03-10 17:18:29 +0800

    Config version

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Eevee'
 author = 'Eevee'
 
 # The short X.Y version
-version = ''
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '{}-alpha'.format(version)
 
 
 # -- General configuration ---------------------------------------------------
